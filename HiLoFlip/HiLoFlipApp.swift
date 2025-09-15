79baee0bfc3e9da7625a3ad187857b87024d9f6d
Aaron Hill <arhill1403@gmail.com>
2025-09-15 19:43:45 -0400

    Initial Commit

---
+++ HiLoFlip/HiLoFlipApp.swift
@@ -0,0 +1,17 @@
+//
+//  HiLoFlipApp.swift
+//  HiLoFlip
+//
+//  Created by Aaron Hill on 9/15/25.
+//
+
+import SwiftUI
+
+@main
+struct HiLoFlipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HiLoFlipTests/HiLoFlipTests.swift
@@ -0,0 +1,17 @@
+//
+//  HiLoFlipTests.swift
+//  HiLoFlipTests
+//
+//  Created by Aaron Hill on 9/15/25.
+//
+
+import Testing
+@testable import HiLoFlip
+
+struct HiLoFlipTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
